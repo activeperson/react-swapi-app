3cfcca48abda8ebce3f3be92ef0392eeea7898a8
Malyshko Vladislav <vladislavm@wizardsdev.com>
2020-03-16 17:52:36 +0200

    added fetch people

--- src/components/SinglePage.js
+++ src/components/SinglePage.js
@@ -3,16 +3,35 @@ import React, { useState, useEffect, Fragment } from 'react';
 
 const SinglePage = (props) => {
     const pageID = props.match.params.id;
+    const [people, setPeople] = useState({people: [], loading: true});
     const [data, setData] = useState({ data: [], loading: true });
+    const [flag, setFlag] = useState({active: false});
 
+    const styles = {
+        img: {
+            width: '85px',
+            height: '85px',
+            objectFit: 'cover'
+        },
+        li: {
+            marginBottom: '25px'
+        },
+        button: {
+            marginBottom: '25px',
+            marginTop: '25px'
+        }
+
+    }
 
     useEffect(() => {
         let fetchFilms = fetch(`https://swapi.co/api/films/${pageID}`).then(response => response.json());
         fetchFilms.then(data => setData({ data, loading: false }));
     }, []);
 
-    const showPeoples = () => {
-
+    const showPeoples = (e) => {
+        (flag.active) ? setFlag({active: false}) : setFlag({active: true});
+        const requestsPeople = data.data.characters.map(url => fetch(url));
+        Promise.all(requestsPeople).then(responses => Promise.all(responses.map(r => r.json()))).then(all => setPeople({people: all, loading: false}));
     }
     return (
         <Fragment>
@@ -41,20 +60,31 @@ const SinglePage = (props) => {
                     </div>
                     <hr />
                     <ul className="list-group">
-                            <li className="list-group-item"><b>Peoples in this film:</b> </li>
+                            <li className="list-group-item"><b>People in this film:</b> </li>
                             <li className="list-group-item">
-                            <button type="button" onClick={showPeoples} className="btn btn-primary">Show peoples</button>
+            <button type="button" style={styles.button} onClick={showPeoples} className="btn btn-primary">{flag.active ? 'Hide' : 'Show'} people</button>
+                            {flag.active ? (
                                 <ul className="list-unstyled">
-                                    {data.data.characters.map(({ title }, index) => (
-                                        <li className="media" key={index + 'EQAWdfretqweqweda'}>
-                                            <img src="" className="mr-3" alt="..." />
-                                            <div className="media-body">
-                                                <h5 className="mt-0 mb-1">List-based media object</h5>
-                                                Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus.
-                                            </div>
-                                        </li>
-                                    ))}
-                                </ul>
+                                    {people.loading ? (
+                                        <div className="spinner-border" role="status"></div>
+                                    ) : (
+                                        console.log(people.people),
+                                        people.people.map(({ name }, index) => (
+                                            <li style={styles.li} className="media" key={index + 'EQAWdfretqweqweda'}>
+                                                <img style={styles.img} src={`https://starwars-visualguide.com/assets/img/characters/${index + 1}.jpg`} className="mr-3" alt={name} />
+                                                <div className="media-body">
+                                                    <h5 className="mt-0 mb-1">{name}</h5>
+                                                    Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus.
+                                                </div>
+                                            </li>
+                                        ))
+                                    )}
+                               
+                            </ul>
+                            ) : (
+                                ''
+                            )}
+                                
                             </li>
                         </ul>
                 </div>
